dcec8dcd5719632adf3dc0bd7b0c354c2de2584f
fabiofallah <136660268+fabiofallah@users.noreply.github.com>
2025-07-10 09:26:39 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,6 +2,7 @@ from flask import Flask
 from telegram import Update, Bot
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 import os
+import subprocess
 
 TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
 bot = Bot(token=TOKEN)
@@ -18,9 +19,23 @@ def home():
 async def ping(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text('pong')
 
+async def gerar_entrada(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("⏳ Gerando entrada, aguarde...")
+
+    try:
+        subprocess.run(['python', 'fallah_drive_send_bot.py'], check=True)
+        subprocess.run(['python', 'gerar_imagem_matriz.py'], check=True)
+
+        chat_id = update.effective_chat.id
+        bot.send_photo(chat_id=chat_id, photo=open('matriz_entrada_preenchida.png', 'rb'))
+
+        await update.message.reply_text("✅ Imagem gerada e enviada com sucesso.")
+    except subprocess.CalledProcessError as e:
+        await update.message.reply_text(f"❌ Erro ao gerar ou enviar a imagem: {e}")
+
 if __name__ == '__main__':
     application = ApplicationBuilder().token(TOKEN).build()
     application.add_handler(CommandHandler('start', start))
     application.add_handler(CommandHandler('ping', ping))
+    application.add_handler(CommandHandler('gerarentrada', gerar_entrada))
     application.run_polling()
-
