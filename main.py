687acfb141d1e24d3b2f6df473c48ecd86c9a927
fabiofallah <136660268+fabiofallah@users.noreply.github.com>
2025-07-16 13:43:37 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 from flask import Flask
+import os
+
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return 'Fallah bot ativo com sucesso!'
-
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
