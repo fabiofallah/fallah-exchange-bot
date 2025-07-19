59fe81fd4990ab8baab2f54e386a50da41648e24
fabiofallah <136660268+fabiofallah@users.noreply.github.com>
2025-07-19 08:40:06 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
-import os, json, gspread
 from google.oauth2.service_account import Credentials
+import gspread, json
 
-GOOGLE_CREDS_JSON = os.getenv("GOOGLE_CREDENTIALS_JSON")
-SPREADSHEET_ID = os.getenv("SPREADSHEET_ID")
-
-scopes = [
-  'https://www.googleapis.com/auth/spreadsheets',
-  'https://www.googleapis.com/auth/drive'
-]
+GOOGLE_CREDS_JSON = os.getenv('GOOGLE_CREDENTIALS_JSON')
 creds_info = json.loads(GOOGLE_CREDS_JSON)
+scopes = [
+  "https://www.googleapis.com/auth/spreadsheets",
+  "https://www.googleapis.com/auth/drive"
+]
 creds = Credentials.from_service_account_info(creds_info, scopes=scopes)
 gc = gspread.authorize(creds)
 
-sh = gc.open_by_key(SPREADSHEET_ID)
-ws = sh.worksheet("Fallah_Clientes_Oficial")
+sh = gc.open_by_key(SPREADSHEET_ID)  # aqui deve funcionar
+ws = sh.worksheet('Fallah_Clientes_Oficial')
